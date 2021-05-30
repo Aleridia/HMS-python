fc401fd31819cc00a8fba5247d099dfe9bd07b84
Aleridia <maxdu34.m@gmail.com>
2021-05-30 17:45:03 +0200

    OUI LUCAS

--- main2.py
+++ main2.py
@@ -9,3 +9,33 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import tensorflow as tf
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+
+df = pd.read_csv('XSens1200.csv')
+
+date_time = pd.to_datetime(df.pop('time_code'), format='%Y/%m/%d - %H:%M:%S:%f')
+
+timestamp = date_time.map(datetime.datetime.timestamp)
+
+column_indices = {name: i for i, name in enumerate(df.columns)}
+
+n = len(df)
+train_df = df[0:int(n*0.7)]
+val_df = df[int(n*0.7):int(n*0.9)]
+test_df = df[int(n*0.9):]
+
+num_features = df.shape[1]
+
+
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+
+
