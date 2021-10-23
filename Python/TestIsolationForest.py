d9f139141e83a24de46c699f4548aa77dc1609ab
PastyBully <terte>
2021-10-23 23:51:21 +0200

    correction script python

--- Python/TestIsolationForest.py
+++ Python/TestIsolationForest.py
@@ -10,47 +10,44 @@ dfApprentissage = pd.read_csv('../data/Apprentissage.csv')
 dfDetection = pd.read_csv('../data/Detection.csv')
 
 # Donnée d'apprentissage
-X_train = pd.DataFrame(np.c_[dfApprentissage['temperature'], dfApprentissage['temperature']], columns=['x1', 'x2'])
+X_train = pd.DataFrame(np.c_[dfApprentissage['temperature']], columns=['x1'])
 # Donnée de détection
-X_test = pd.DataFrame(np.c_[dfDetection['temperature'], dfDetection['temperature']], columns=['x1', 'x2'])
+X_test = pd.DataFrame(np.c_[dfDetection['temperature']], columns=['x1'])
 
 # training the model
 clf = IsolationForest(max_samples=20, max_features=1.0)
 
-clf.fit(X_train)
+clf.fit(X_train.values)
 # On prédit
-dfDetection['anomaly'] = clf.predict(X_test)
+dfDetection['anomaly'] = clf.predict(X_test.values)
 
-print(X_test)
 
 # On récupère les anomalies
 nb_test = 0
 for i in range(len(dfDetection['anomaly'])):
-    print( dfDetection['anomaly'][i], i)
     if dfDetection['anomaly'][i] != 1:
         nb_test += 1
 
 print("Nombre anomalie dans la base de test = " + str(nb_test) + "/" + str(len(X_test)))
 # ----------------------------------------------------
 
-xx, yy = np.meshgrid(np.linspace(20, 30, 50), np.linspace(20, 30, 50))
-Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-
-plt.title("IsolationForest")
-plt.contourf(xx, yy, Z, cmap=plt.cm.Blues_r)
-print(X_train.x1)
-b1 = plt.scatter(X_train.x1, X_train.x2, c='red',
-                 s=20, edgecolor='k')
-b2 = plt.scatter(X_test.x1, X_test.x2, c='green',
-                 s=20, edgecolor='k')
-# c = plt.scatter(X_outliers[:, 0], X_outliers[:, 1], c='red',
-#                 s=20, edgecolor='k')
-plt.axis('tight')
-plt.xlim((26, 30))
-plt.ylim((26, 30))
-plt.legend([b1, b2],
-           ["Apprentissage",
-            "Détection"],
-           loc="upper left")
-plt.show()
+# xx, yy = np.meshgrid(np.linspace(20, 30, 50), np.linspace(20, 30, 50))
+# Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+# Z = Z.reshape(xx.shape)
+#
+# plt.title("IsolationForest")
+# plt.contourf(xx, yy, Z, cmap=plt.cm.Blues_r)
+# b1 = plt.scatter(X_train.x1, X_train.x2, c='red',
+#                  s=20, edgecolor='k')
+# b2 = plt.scatter(X_test.x1, X_test.x2, c='green',
+#                  s=20, edgecolor='k')
+# # c = plt.scatter(X_outliers[:, 0], X_outliers[:, 1], c='red',
+# #                 s=20, edgecolor='k')
+# plt.axis('tight')
+# plt.xlim((26, 30))
+# plt.ylim((26, 30))
+# plt.legend([b1, b2],
+#            ["Apprentissage",
+#             "Détection"],
+#            loc="upper left")
+# plt.show()
--- Python/TestIsolationForest_socket.py
+++ Python/TestIsolationForest_socket.py
@@ -8,7 +8,10 @@ import os, sys, socket, time
 data = dict()
 """
 Fonction d'apprentissage
+
 """
+
+
 def learning():
     dfApprentissage = pd.read_csv('../data/Apprentissage.csv')
 
@@ -22,8 +25,8 @@ def learning():
 
         X_train = pd.DataFrame(np.c_[dfApprentissage[elem]], columns=['x1'])
 
-        clf = IsolationForest(max_samples=20)
-        clf.fit(X_train)
+        clf = IsolationForest(max_samples=1)
+        clf.fit(X_train.values)
 
         object[elem] = clf
 
@@ -32,23 +35,24 @@ def learning():
 
 # --------Detection---------
 
-def detection():
+def detection(object):
+    anomaly = dict()
     for cle, valeur in data.items():
+        if len(valeur) == 0:
+            continue
+
         X_test = pd.DataFrame(np.c_[valeur], columns=['x1'])
         # On prédit
-        dfDetection['anomaly'] = object[cle].predict(X_test)
-
+        anomaly['anomaly'] = object[cle].predict(X_test.values)
         # On récupère les anomalies
         nb_test = 0
-        for i in range(len(dfDetection['anomaly'])):
-            if dfDetection['anomaly'][i] != 1:
+        for i in range(len(anomaly['anomaly'])):
+            if anomaly['anomaly'][i] != 1:
                 nb_test += 1
-
         print("Nombre anomalie dans la base = " + str(nb_test) + "/" + str(len(X_test)))
 
         # On vide la liste pour la prochaine rotation
         data[cle].clear()
-
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("127.0.0.1", 2525))
@@ -57,8 +61,11 @@ s.listen(5)
 
 def parse(data):
     data = data.split(';')
-    return (data[0], data[1])
 
+    if len(data) < 2:
+        return (None, None)
+
+    return (data[0], data[1])
 
 object = learning()
 start = time.time()
@@ -72,25 +79,30 @@ try:
         read = 0
 
         while True:
-            chunk = client.recv(128)
+            chunk = client.recv(256)
             if chunk == b'':
                 break
-
-            if (end - start) >= 10:
-                detection()
+            if (end - start) >= 2:
+                detection(object)
                 start = time.time()
 
             end = time.time()
-
             messages = chunk.decode('ascii')
             messages = messages.split('|')
 
+            nbNewElement = 0
             for message in messages[:-1]:
-                (value, sensor) = parse(message[:-1])
+                (value, sensor) = parse(message)
+                if value is None:
+                    continue
 
+                nbNewElement += 1
                 data[sensor].append(value)
 
+            print("Réception de " + str(nbNewElement) + " nouveaux éléments")
+            time.sleep(5)
+
 except Exception as ex:
-    print(ex)
+    print("Exception:" + str(ex))
 finally:
     s.close()
